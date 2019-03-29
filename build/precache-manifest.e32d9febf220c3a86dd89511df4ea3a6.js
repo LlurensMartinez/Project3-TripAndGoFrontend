4ce4df541df786dcfa03d0816677edbc7983a74f
Miguel Sanchez <migsandia@gmail.com>
2019-03-29 11:57:06 +0100

    update env.production

---
+++ build/precache-manifest.e32d9febf220c3a86dd89511df4ea3a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b261551f77184f276db8",
+    "url": "/static/css/main.7ac0b2f1.chunk.css"
+  },
+  {
+    "revision": "b261551f77184f276db8",
+    "url": "/static/js/main.b261551f.chunk.js"
+  },
+  {
+    "revision": "01399b2ddcc00d82bcba",
+    "url": "/static/js/1.01399b2d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5c2b45bb0a54ba3c7547b154913fb39",
+    "url": "/index.html"
+  }
+];
